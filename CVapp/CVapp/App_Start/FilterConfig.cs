023f89dea18e446e77b0e625ad86ef626a46d537
Johnny <19-39366-1@student.aiub.edu>
2022-02-11 23:51:57 +0600

    first CV Task

---
+++ CVapp/CVapp/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CVapp
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ CVapp/CVapp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CVapp.MvcApplication" Language="C#" %>
